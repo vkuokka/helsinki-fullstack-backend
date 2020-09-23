727c954da753840cf1348236ec7dc1844c5e57a1
Vesa Kuokka <vkuokka@student.hive.fi>
2020-09-23 17:51:39 +0300

    3.12: tietokanta komentorivilta

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+	console.log('password required as argument')
+	process.exit(1)
+}
+
+const password = process.argv[2]
+const dbname = 'fullstack'
+const url = `mongodb+srv://test:${password}@cluster0.bin5b.mongodb.net/${dbname}?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const personSchema = new mongoose.Schema({
+	name: String,
+	number: String
+})
+
+const Person = mongoose.model('person', personSchema)
+
+if (process.argv.length > 3) {
+	const person = new Person({
+		name: process.argv[3],
+		number: process.argv[4]
+	})
+	person.save()
+		.then(result => {
+			console.log(`added ${person.name} number ${person.number} to phonebook`)
+			mongoose.connection.close();
+		})
+}
+else {
+	console.log('phonebook:')
+	Person.find({})
+		.then(result => {
+			result.forEach(person => {
+			console.log(`${person.name} ${person.number}`)
+			mongoose.connection.close()
+		})
+	})
+}
