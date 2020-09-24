1351d256bd8b12775fd2c38bede1e0e1d0a2e815
Vesa Kuokka <vkuokka@student.hive.fi>
2020-09-24 12:46:54 +0300

    3.14: puhelinluettelo ja tietokanta

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const { response } = require('express')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
 morgan.token('content', (request) => {
 	return JSON.stringify(request.body)
@@ -13,93 +15,99 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-let persons =  [
-	{
-		"name": "Arto Hellas",
-		"number": "040-123456",
-		"id": 1
-	},
-	{
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523",
-		"id": 2
-	},
-	{
-		"name": "Dan Abramov",
-		"number": "12-43-234345",
-		"id": 3
-	},
-	{
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122",
-		"id": 4
-	}
-]
+// let persons =  [
+// 	{
+// 		"name": "Arto Hellas",
+// 		"number": "040-123456",
+// 		"id": 1
+// 	},
+// 	{
+// 		"name": "Ada Lovelace",
+// 		"number": "39-44-5323523",
+// 		"id": 2
+// 	},
+// 	{
+// 		"name": "Dan Abramov",
+// 		"number": "12-43-234345",
+// 		"id": 3
+// 	},
+// 	{
+// 		"name": "Mary Poppendieck",
+// 		"number": "39-23-6423122",
+// 		"id": 4
+// 	}
+// ]
 
-const createId = () => {
-	return Math.floor(Math.random() * Math.floor(1000));
-	// return persons.length > 0
-	// ? Math.max(...persons.map(person => person.id)) + 1
-	// : 0
-}
+// const createId = () => {
+// 	return Math.floor(Math.random() * Math.floor(1000));
+// 	// return persons.length > 0
+// 	// ? Math.max(...persons.map(person => person.id)) + 1
+// 	// : 0
+// }
 
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({})
+		.then(result => response.json(result))
+		.catch(error => response.status(404).end)
 })
 
 app.get('/info', (request, response) => {
-	response.send(`
-	<div>
-	<p>Phonebook has info for ${persons.length} people</p>
-	<p>${new Date()}</p>
-	</div>`
-	)
+	Person.count().then(result => {
+		response.send(`
+			<div>
+			<p>Phonebook has info for ${result} people</p>
+			<p>${new Date()}</p>
+			</div>`
+		)
+	}).catch(error => response.status(404).end)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	Person.findById(request.params.id).then(result => {
+		response.json(result)
+	}).catch(error => response.status(404).end)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-	response.status(204).end()
+	Person.findByIdAndRemove(request.params.id).then(result => {
+		response.status(204).end()
+	}).catch(error => response.status(404).end())
 })
 
 app.post('/api/persons/', (request, response) => {
-	const person = request.body
-	if (!person) {
+	const body = request.body
+	if (!body) {
 		return response.status(400).json({
 			error: 'content missing'
 		})
 	}
-	if (!person.name) {
+	if (!body.name) {
 		return response.status(400).json({
 			error: 'name missing'
 		})
 	}
-	if (!person.number) {
+	if (!body.number) {
 		return response.status(400).json({
 			error: 'number missing'
 		})
 	}
-	if (persons.find(x => x.name === person.name)) {
-		return response.status(400).json({
-			error: "name must be unique"
+	Person.find({}).then(result => {
+		if (result.find(person => person.name == body.name)) {
+	 		return response.status(400).json({
+	 			error: "name must be unique"
+			})
+		}
+		// person.id = createId();
+		// persons = persons.concat(person)
+		person = new Person ({
+			name: body.name,
+			number: body.number
 		})
-	}
-	person.id = createId();
-	persons = persons.concat(person)
-	response.json(person)
+		person.save(person).then(result => response.json(result))
+	}).catch(error => response.status(404).end)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
